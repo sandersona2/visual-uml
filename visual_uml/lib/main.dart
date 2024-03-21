d9d64d4a2cf937e1e9e73d269e469e8104ca9e7a
Peter Garrity <garrityp@wit.edu>
2024-03-21 06:30:46 -0400

    Add draggable to dynamically add widgets

--- visual_uml/lib/main.dart
+++ visual_uml/lib/main.dart
@@ -20,6 +20,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class TargetArea extends StatelessWidget {
+  const TargetArea();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+
+    );
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -30,24 +41,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Widget> a = [
-      Container(
-        height: 200,
-        child: Text('test'),
-      ),
-    ];
+  List<Widget> a = [];
 
   void _d() {
     setState(() {
-      a.add(Container(
+      a.addAll([SizedBox(
               height: 200,
-              child: Text('test'),
+              width: 100,
+              child: Card(
+                color: Theme.of(context).colorScheme.primary,
+                child: Center(
+                  child: Text('test',
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  )
+                ),
+              ),
             ),
+            DragTarget<List<Widget>>(
+              builder: (
+                BuildContext context,
+                List<dynamic> accepted,
+                List<dynamic> rejected,
+              ) {
+                return SizedBox(
+                  height: 200,
+                  width: 50,
+                  child: Card(color: accepted.isNotEmpty ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary),
+                );
+              },
+              onAcceptWithDetails: (details) {
+                setState(() {
+                  a.addAll(details.data);
+                });
+              },
+            ),]
       );
     });
   }
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -57,23 +89,89 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            TextButton(
-              onPressed: () {
-                _d();
-              },
-              child: Text('Press here to add item')
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: OverflowBar(
-                
-                children: a,
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Draggable<List<Widget>>(
+                  data: [SizedBox(
+                    height: 200,
+                    width: 100,
+                    child: Card(
+                      color: Theme.of(context).colorScheme.primary,
+                      child: Center(
+                        child: Text('test',
+                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                          ),
+                        )
+                      ),
+                    ),
+                  ),
+                  DragTarget<List<Widget>>(
+                    builder: (
+                      BuildContext context,
+                      List<dynamic> accepted,
+                      List<dynamic> rejected,
+                    ) {
+                      return SizedBox(
+                        height: 200,
+                        width: 50,
+                        child: Card(color: accepted.isNotEmpty ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary),
+                      );
+                    },
+                    onAcceptWithDetails: (details) {
+                      setState(() {
+                        a.addAll(details.data);
+                      });
+                    },
+                  ),
+                ],
+                feedback: SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Card(
+                    color: Theme.of(context).colorScheme.primary,
+                    child: Center(
+                      child: Text('drag',
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onPrimary,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                child: SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Card(
+                    color: Theme.of(context).colorScheme.primary,
+                    child: Center(
+                      child: Text('drag',
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onPrimary,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  _d();
+                },
+                child: Text('Press here to add item')
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: OverflowBar(
+                  
+                  children: a,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
