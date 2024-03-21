fef5a06932e5fc34f27bd87f64325089ff61ff61
Peter Garrity <garrityp@wit.edu>
2024-03-21 14:45:20 -0400

    Redid layout

--- visual_uml/lib/main.dart
+++ visual_uml/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,17 +21,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ItemSource extends StatelessWidget {
-  const ItemSource();
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-
-    );
-  }
-}
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -43,44 +33,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<Widget> a = [];
 
-  void _d() {
-    setState(() {
-      a.addAll([SizedBox(
-              height: 200,
-              width: 100,
-              child: Card(
-                color: Theme.of(context).colorScheme.primary,
-                child: Center(
-                  child: Text('test',
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),
-                  )
-                ),
-              ),
-            ),
-            DragTarget<List<Widget>>(
-              builder: (
-                BuildContext context,
-                List<dynamic> accepted,
-                List<dynamic> rejected,
-              ) {
-                return SizedBox(
-                  height: 200,
-                  width: 50,
-                  child: Card(color: accepted.isNotEmpty ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary),
-                );
-              },
-              onAcceptWithDetails: (details) {
-                setState(() {
-                  a.addAll(details.data);
-                });
-              },
-            ),]
-      );
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     
@@ -89,91 +41,102 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: Column(
-          children: [
-            Draggable<List<Widget>>(
-                data: [SizedBox(
-                  height: 200,
-                  width: 100,
-                  child: Card(
-                    color: Theme.of(context).colorScheme.primary,
-                    child: Center(
-                      child: Text('test',
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Theme.of(context).colorScheme.onPrimary,
+      body: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: ListView(
+                children: [Draggable<List<Widget>>(
+                  data: [SizedBox(
+                      width: 100,
+                      child: Card(
+                        color: Theme.of(context).colorScheme.primary,
+                        child: Center(
+                          child: Text('test',
+                            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                          )
                         ),
-                      )
+                      ),
                     ),
-                  ),
-                ),
-                DragTarget<List<Widget>>(
-                  builder: (
-                    BuildContext context,
-                    List<dynamic> accepted,
-                    List<dynamic> rejected,
-                  ) {
-                    return SizedBox(
-                      height: 200,
-                      width: 50,
-                      child: Card(color: accepted.isNotEmpty ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary),
-                    );
-                  },
-                  onAcceptWithDetails: (details) {
-                    setState(() {
-                      a.addAll(details.data);
-                    });
-                  },
-                ),
-              ],
-              feedback: SizedBox(
-                height: 100,
-                width: 100,
-                child: Card(
-                  color: Theme.of(context).colorScheme.primary,
-                  child: Center(
-                    child: Text('drag',
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        color: Theme.of(context).colorScheme.onPrimary,
+                    DragTarget<List<Widget>>(
+                      builder: (
+                        BuildContext context,
+                        List<dynamic> accepted,
+                        List<dynamic> rejected,
+                      ) {
+                        return SizedBox(
+                          width: 50,
+                          child: Card(color: accepted.isNotEmpty ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary),
+                        );
+                      },
+                      onAcceptWithDetails: (details) {
+                        setState(() {
+                          a.addAll(details.data);
+                        });
+                      },
+                    ),
+                  ],
+                  feedback: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: Card(
+                      color: Theme.of(context).colorScheme.primary,
+                      child: Center(
+                        child: Text('drag',
+                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              child: SizedBox(
-                height: 100,
-                width: 100,
-                child: Card(
-                  color: Theme.of(context).colorScheme.primary,
-                  child: Center(
-                    child: Text('drag',
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        color: Theme.of(context).colorScheme.onPrimary,
+                  child: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: Card(
+                      color: Theme.of(context).colorScheme.primary,
+                      child: Center(
+                        child: Text('drag',
+                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ),]
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                _d();
+          ),
+          Expanded(
+            flex: 3,
+            child: DragTarget<List<Widget>>(
+              builder: (
+                BuildContext context,
+                List<dynamic> accepted,
+                List<dynamic> rejected,
+              ) {
+                return Container(
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: a.length,
+                    itemBuilder: (context, index) {
+                      return a[index];
+                    },
+                  ),
+                );
               },
-              child: Text('Press here to add item')
+              onAcceptWithDetails: (details) {
+                setState(() {
+                  a.addAll(details.data);
+                });
+              },
             ),
-            SizedBox(
-              height: 200,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: a.length,
-                itemBuilder: (context, index) {
-                  return a[index];
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
